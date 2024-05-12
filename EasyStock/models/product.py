b48dd63006d0dd6efa453f8762eb1a1524d42fda
natia02 <pruidze.natia@kiu.edu.ge>
2024-05-12 21:41:53 +0400

    change the order of the fields

--- EasyStock/models/product.py
+++ EasyStock/models/product.py
@@ -3,13 +3,13 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name=_('Product Name'))
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Price'))
-    quantity = models.IntegerField(verbose_name=_('Quantity'))
     categories = models.ManyToManyField(
         to='EasyStock.Category',
         related_name='products',
         verbose_name=_('Categories'))
+    name = models.CharField(max_length=100, verbose_name=_('Product Name'))
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Price'))
+    quantity = models.IntegerField(verbose_name=_('Quantity'))
 
     def __str__(self):
         return self.name
